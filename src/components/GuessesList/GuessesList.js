eb431bce3083465197e44006f843b558493670d9
i.colussi <yuliabelova2307@gmail.com>
2023-08-16 16:20:48 +0200

    finished the third round

--- src/components/GuessesList/GuessesList.js
+++ src/components/GuessesList/GuessesList.js
@@ -1,12 +1,16 @@
 import React from "react";
+import {range} from "./../../utils";
+import {NUM_OF_GUESSES_ALLOWED} from "./../../constants";
+import Guess from "./../Guess/index";
 
 function GuessesList({guessesList}) {
   return(
-      <div className="guess-results">
-          {guessesList.map(({value, id}) =>
-          <p className="guess" id={id}>{value}</p>
+    <div className="guess-results">
+        {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+            <Guess key={num} value={guessesList[num]}/>
+            )
         )}
-      </div>
+    </div>
   );
 }
 
